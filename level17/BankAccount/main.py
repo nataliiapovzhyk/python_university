57c08888069a6798bd21b49660f6a7a362cedb6a
Nataliia Povzhyk <nataliiapovzhyk@gmail.com>
2026-02-03 16:38:54 +0200

    added main

--- level17/BankAccount/main.py
+++ level17/BankAccount/main.py
@@ -5,15 +5,36 @@ from operations import withdraw
 
 
 def start_banking():
-    print("Start Banking")
-    account_one = Account(1,"Test Name")
     try:
-        deposit(account_one,600)
-        deposit(account_one, 200)
+        account_one = Account(1, "Test Name")
 
-        withdraw(account_one, 100)
-        withdraw(account_one, 300)
-        withdraw(account_one, 700)
+        isConnect = True
+
+        while isConnect:
+            print("")
+            print("============")
+            print("Hello to Bank Account:")
+            print("1. Deposit Account")
+            print("2. Withdraw Account")
+            print("3. Show Balance")
+            print("4. Exit")
+            print("============")
+            print("")
+
+            selected_menu = int(input("Select a menu option: "))
+            if selected_menu == 1:
+                value = int(input("Enter a sum to deposit: "))
+                deposit(account_one, value)
+            elif selected_menu == 2:
+                value = int(input("Enter a sum to withdraw: "))
+                withdraw(account_one, value)
+            elif selected_menu == 3:
+                print(account_one)
+            elif selected_menu == 4:
+                isConnect = False
+
+    except ValueError as e:
+        print(e)
     except InvalidAmountError as e:
         print(e)
     except InsufficientFundsError as e:
